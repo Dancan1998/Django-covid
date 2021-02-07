12a9801d1cf7b41feb74e837c409d82b4966f52f
Dancan Chibole <dancankingstar@gmail.com>
2021-02-07 14:30:41 -0800

    changed the settings

--- config/settings.py
+++ config/settings.py
@@ -132,12 +132,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dancankingstar@gmail.com'
+EMAIL_HOST_PASSWORD = 'shanghai2021'
+EMAIL_USE_TLS =EMAIL_USE_TLS=True
+
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
